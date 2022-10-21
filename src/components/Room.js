1f23d7ae204c23b559776092423cef4e4daefd1a
Marcelearn <jaworskimarcel@gmail.com>
2022-10-21 21:31:24 +0200

    final

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,18 +1,5 @@
 import { useState } from "react";
-// export default function Room() {
-//     const [stateMode, setStateMode] = useState(false);
-//     const lit = "The room is light";
-//     const dark = "The room is dark";
-//     const evenClick = () => {
-//       setStateMode(!stateMode);
-//     };
-//     return (
-//       <div className={stateMode ? lit : dark}>
-//         <h1> {stateMode ? lit : dark}</h1>
-//         <button onClick={evenClick}>{stateMode ? "Light" : "dark"}</button>
-//       </div>
-//     );
-//   }
+
 export default function Room() {
     const [lightStatus, setLight] = useState(false);
     const light = 'The room light';
